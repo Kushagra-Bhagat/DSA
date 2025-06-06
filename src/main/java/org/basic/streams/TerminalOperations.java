25bdcea8eb6722a61a3ec600165592e495e43cf9
Kushagra Bhagat <bhagatkushagra@gmail.com>
2025-06-07 02:06:54 +0530

    Added terminal operations file

---
+++ src/main/java/org/basic/streams/TerminalOperations.java
@@ -0,0 +1,45 @@
+package org.basic.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class TerminalOperations {
+    public static void main(String[] args) {
+        // they terminate the stream
+
+        // 1. collect()
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
+        // can use toList() for newer v ersion of java
+        List<Integer> collect = nums.stream().filter(x -> x % 2 == 0).toList();
+        List<Integer> collect1 = nums.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+
+        // 2. forEach()
+        nums.stream().forEach(System.out::println);
+
+        // 3. reduce -> combines elements to produce a single result
+        Optional<Integer> reduce = nums.stream().reduce((x, y) -> x + y);
+        System.out.println(reduce.get());
+        Optional<Integer> reduce1 = nums.stream().reduce(Integer::sum);
+        System.out.println(reduce1.get());
+
+        // 4. anyMatch, allMatch, noneMatch
+        boolean anyMatch = nums.stream().anyMatch(x -> x % 2 == 0);
+        boolean allMatch = nums.stream().allMatch(x -> x > 0);
+        boolean noneMatch = nums.stream().noneMatch(x -> x < 0);
+
+        // 5. findAny, findFirst
+        Optional<Integer> first = nums.stream().findFirst();
+        Optional<Integer> any = nums.stream().findAny();
+
+
+        // Examples
+        // Counting occurence of characters
+        String s = "hello world";
+        // even if we make char array we can't convert char array to stream
+        // we have a special function to convert string to stream
+
+        System.out.println(s.chars().filter(x -> x == 'l').count());
+    }
+}
